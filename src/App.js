5f61f2a12aa16bf7f9fe994d0c7ed89fdb5e8889
김동현 <ehakck7@gmail.com>
2022-11-30 10:13:03 +0900

    5장 스터디 관련 커밋

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ import CounterClass from "./03/CounterClass";
 import Say from "./03/Say";
 import EventPracticeClass from "./04/EventPracticeClass";
 import EventPractice from "./04/EventPractice";
+import ValidationSampleClass from "./05/ValidationSampleClass";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <EventPractice />
-      <EventPracticeClass />
-      <MyComponents name={"test"}>나는 children 이다</MyComponents>
-      <MyComponentsClass name={"클래스 형"}>클래스 child</MyComponentsClass>
-      <CounterClass />
-      <Say />
-    </div>
-  );
+class App extends Component {
+  validation = React.createRef();
+
+  render() {
+    return (
+      <div>
+        <ValidationSampleClass ref={this.validation} />
+        <button onClick={() => this.validation.current.handleFocus()}>
+          input 포커스
+        </button>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <ValidationSampleClass />
+//       <EventPractice />
+//       <EventPracticeClass />
+//       <MyComponents name={"test"}>나는 children 이다</MyComponents>
+//       <MyComponentsClass name={"클래스 형"}>클래스 child</MyComponentsClass>
+//       <CounterClass />
+//       <Say />
+//     </div>
+//   );
+// }
 
 export default App;
